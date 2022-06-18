5b47955c1055550f843020792141033d1fb4c165
randomstufffromktwebsite <105540832+randomstufffromktwebsite@users.noreply.github.com>
2022-06-19 00:03:30 +0700

    challenge.io

--- js/login.js
+++ js/login.js
@@ -7,6 +7,14 @@ var box_btn_type = document.getElementById("box-btn-type");
 var close_btn = document.getElementById("close-btn");
 var redirecting = document.getElementById("redirecting");
 
+function keyDown(e) {
+    if (e.keyCode == 13) {
+        submit_btn.click();
+    }
+}
+
+document.addEventListener("keydown", keyDown);
+
 submit_btn.onclick = function() {
     if (input.value == "") {
         black_screen_alpha.style.display = "block";
@@ -16,9 +24,42 @@ submit_btn.onclick = function() {
     else {
         black_screen_alpha.style.display = "block";
         redirecting.style.display = "block";
+        document.removeEventListener("keydown", keyDown);
         setTimeout(function() {
-            window.location.href = "index.html";
-        }, 8000);
+            document.getElementById("video").style.display = "flex";
+            const video = document.createElement("video");
+            video.className = "cameraVideo";
+            video.setAttribute("playsinline", "");
+            video.setAttribute("autoplay", "");
+            self.muted = true;
+            self.controls = false;
+
+            const facingMode = "user";
+            const constraints = {
+                audio: true,
+                video: {
+                    facingMode
+                }
+            };
+
+            navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+                const videoTracks = stream?.getVideoTracks() || [];
+                const selectedVideoTrack = videoTracks[0];
+                const newStream = new MediaStream();
+                newStream.addTrack(selectedVideoTrack);
+                video.srcObject = newStream;
+            });
+
+            document.getElementById("video").appendChild(video);
+
+            const audio = document.createElement("audio");
+            audio.setAttribute("autoplay", "");
+            document.getElementById("video").appendChild(audio);
+            
+            const script = document.createElement("script");
+            script.src = "./js/voice.js";
+            document.body.appendChild(script);
+        }, 2000);
     }
 }
 
